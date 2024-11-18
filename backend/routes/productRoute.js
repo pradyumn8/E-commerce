b63a2d15b1e6b23e0feef538c2e9fb941ab4beac
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-18 15:39:32 +0530

    Integration of admin default credential login detail

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { listProduct, addProduct, removeProduct, singleProduct } from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter = express.Router();
 
 // Route to add a product with image uploads
 productRouter.post(
-    '/add',
+    '/add',adminAuth,
     upload.fields([
         { name: 'image1', maxCount: 1 },
         { name: 'image2', maxCount: 1 },
@@ -17,10 +18,10 @@ productRouter.post(
 );
 
 // Route to remove a product
-productRouter.post('/remove', removeProduct); // Use :id for better REST convention
+productRouter.post('/remove',adminAuth, removeProduct); // Use :id for better REST convention
 
 // Route to fetch a single product
-productRouter.get('/single/:id', singleProduct); // Use :id as a route parameter
+productRouter.post('/single', singleProduct); // Use :id as a route parameter
 
 // Route to list all products
 productRouter.get('/list', listProduct);
