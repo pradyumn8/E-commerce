4f5ec0385ae83330892a63a5df06191b9ddddfd4
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-19 11:08:35 +0530

    using local storage for admin when refresh the page, can still login

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Routes, Route } from 'react-router-dom'
@@ -13,7 +13,11 @@ export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
 
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+
+  useEffect(()=>{
+    localStorage.setItem('token',token)
+  },[token])
 
   return (
     <div className='bg-gray-50 min-h-screen'>
