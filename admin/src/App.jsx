1bdc642b68e4b09bb09043aa5c5edcd536ceee4f
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-19 12:31:21 +0530

    prouct add UI is completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -25,15 +25,15 @@ const App = () => {
       {token === ""
         ? <Login setToken={setToken}/>
         : <>
-          <Navbar />
+          <Navbar setToken={setToken} />
           <hr />
           <div className="flex w-full">
             <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path='/add' element={<Add />} />
-                <Route path='/list' element={<List />} />
-                <Route path='/order' element={<Order />} />
+                <Route path='/add' element={<Add token={token} />} />
+                <Route path='/list' element={<List token={token} />} />
+                <Route path='/order' element={<Order token={token} />} />
               </Routes>
             </div>
           </div>
